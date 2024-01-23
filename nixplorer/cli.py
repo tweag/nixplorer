213acf8429c1a14d104d8113ac8cd6b0b911f312
Dorran Howell <dorran.howell@tweag.io>
2024-01-23 15:14:30 +0100

    Refactor graph ingestion logic
    Refactored graph ingestion logic into a dedicated module with a cleaner
    implementation.

--- nixplorer/cli.py
+++ nixplorer/cli.py
@@ -1,55 +1,15 @@
 from pathlib import Path
 from typing import Tuple
-from gremlin_python.process.graph_traversal import GraphTraversalSource
 from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from nixtract.model import Derivation
 import time
 import click
 from halo import Halo
+from nixplorer.ingest import load_graph
 
 from nixplorer.process.graph import Janusgraph
 from nixplorer.process.ui import GraphExplorer
-
-
-def load_graph(derivations: list[Derivation], g: GraphTraversalSource) -> None:
-    # TODO: It might make sense to do a bit of batching here to improve performance,
-    # only calling iterate after N derivations have been processed.
-    for drv in derivations:
-        if drv.output_path is None:
-            continue
-        t = (
-            g.add_v("derivation")
-            .property("output_path", drv.output_path)
-            .property("name", drv.name)
-        )
-        if drv.nixpkgs_metadata:
-            if drv.nixpkgs_metadata.pname is not None:
-                t = t.property("pname", drv.nixpkgs_metadata.pname)
-            if drv.nixpkgs_metadata.description is not None:
-                t = t.property("description", drv.nixpkgs_metadata.description)
-            if drv.nixpkgs_metadata.license is not None:
-                t = t.property("license", drv.nixpkgs_metadata.license)
-            if drv.nixpkgs_metadata.version is not None:
-                t = t.property("version", drv.nixpkgs_metadata.version)
-        t.iterate()
-    # Then in our second pass we draw edges between vertices. This assumes that all vertices involved
-    # in the edges were created in our first pass, otherwise the edge will not be created.
-    for drv in derivations:
-        for bi in drv.build_inputs:
-            if bi.output_path is None:
-                continue
-            t = (
-                g.V()
-                .has("output_path", drv.output_path)
-                .as_("a")
-                .V()
-                .has("output_path", bi.output_path)
-                .add_e("has_build_input")
-                .from_("a")
-            )
-            # _echo(t.bytecode)
-            t.iterate()
 
 
 def read_graph_jsonl(input_graph: Path) -> Tuple[list[Derivation], list[Exception]]:
