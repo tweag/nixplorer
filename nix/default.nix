cab8efd46f9f759b6e2446f490fa62974c7f5059
Dorran Howell <dorran.howell@tweag.io>
2024-01-25 10:22:30 +0100

    Add default flake attribute

--- nix/default.nix
+++ nix/default.nix
@@ -4,4 +4,5 @@
 }: rec {
   graph-explorer = pkgs.callPackage ./aws-graph-explorer.nix {http-server = pkgs.nodePackages.http-server;};
   nixplorer = pkgs.callPackage ./nixplorer.nix {inherit mkPoetryApplication graph-explorer;};
+  default = nixplorer;
 }
